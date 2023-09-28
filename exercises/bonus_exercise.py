61480a10ce25be561e360c470c0d176fc35b5f97
Riki <rriikkiii1221@gmail.com>
2023-09-28 13:13:22 +0800

    ex1

--- exercises/bonus_exercise.py
+++ exercises/bonus_exercise.py
@@ -4,10 +4,13 @@ def print_pyramid():
     #   *
     #  ***
     # *****
-    print("  *")
-    print(" ***")
-    print("*****")
-
+    n=int (input ("enter the number of rows: ") )
+    for i in range (n) :
+        for j in range (n-i-1) :
+            print(" ", end="")
+        for j in range (2*i+1):
+            print("*", end="")
+        print ()
 
 def main():
     print_pyramid()
